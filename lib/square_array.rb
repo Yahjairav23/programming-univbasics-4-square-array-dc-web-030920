5747888ec6382bb796f62bbbd3fab99e2edef7b1
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-03 07:21:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,9 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do
-    puts array[counter]**2
+    new_number = array[counter]**2
+    puts new_number
+    new_number
     counter += 1
   end
 end
