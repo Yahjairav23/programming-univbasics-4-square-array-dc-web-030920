eb91113a4a33a4c30f46836c49855e317295f9dc
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-03 07:11:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,9 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do
-    p Math.sqrt(array[counter])
+    new_number = Math.sqrt(array[counter])
+    puts new_number
+    new_number
     counter += 1
   end
 end
